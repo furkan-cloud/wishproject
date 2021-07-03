9b3f41da90cbbf10125cf5f5440b4d0cbbfe901e
furkan-cloud <firstinfirstout2020@gmail.com>
2021-07-04 02:04:24 +0300

    tailwind, typescript, and nextjs configs added

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="text-red-800">Hello</h1>
+      </main>
+    </div>
+  )
+}
